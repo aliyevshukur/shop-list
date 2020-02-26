3af760f477fd5052e4af26d27a0d6da966f8b40a
Shukur05 <shukuralyv@gmail.com>
2020-02-26 19:04:55 +0400

    js homework 4 fixed

--- JS_Homework4/js/index.js
+++ JS_Homework4/js/index.js
@@ -1,5 +1,7 @@
 function filterBy(arr, type) {
-    return arr.filter((item) => {typeof (item) !== type;});
+    return arr.filter((item) => {
+        return typeof (item) !== type;
+    });
 }
 
 console.log(filterBy(['hello', 'world', 23, '23', null], 'string'));
