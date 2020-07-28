0b7c91e448181b5c395afd4f42067061fa2367ee
Shukur <shukuralyv@gmail.com>
2020-07-28 22:52:00 +0400

    import shop-list

--- JS_Homework4/js/index.js
+++
@@ -1,7 +0,0 @@
-function filterBy(arr, type) {
-    return arr.filter((item) => {
-        return typeof (item) !== type;
-    });
-}
-
-console.log(filterBy(['hello', 'world', 23, '23', null], 'string'));
